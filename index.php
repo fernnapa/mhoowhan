3c3817e50fbdea12e9a1716122440e816af0cc95
Fernn <fernnapaporn8199@gmail.com>
2018-12-26 16:18:08 +0700

    kuy

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ include ("slider.php");
                                     <td>ชื่อ</td>  
                                     <td>หน่วยงาน</td>  
                                     <td>สถานะ</td>  
-                                    <td>action</td> 
+                                    <td>action_ttttt</td> 
                                </tr>  
                           </thead>  
                           <?php  
