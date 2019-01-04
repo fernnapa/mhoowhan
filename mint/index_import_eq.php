b59df5c7c9546a3ba4fcc0a2307defdbe6961749
Mint Mins309 <minn.5757@gmail.com>
2019-01-04 18:58:48 +0700

    imports

--- mint/index_import_eq.php
+++ mint/index_import_eq.php
@@ -9,9 +9,9 @@
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <meta name="description" content="Import Excel File To MySql Database Using php">
 
-          <link rel="stylesheet" href="css/bootstrap.min.css">
-          <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-          <link rel="stylesheet" href="css/bootstrap-custom.css">
+          <link rel="stylesheet" href="css/bootstrap.min-importeq.css">
+          <link rel="stylesheet" href="css/bootstrap-responsive.min-importeq.css">
+          <link rel="stylesheet" href="css/bootstrap-custom-importeq.css">
 
 
 </head>
